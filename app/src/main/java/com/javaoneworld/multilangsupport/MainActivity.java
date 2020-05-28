9fcfa0186314c0b8c5103f62e7a94f7bd0cbc155
amit mishra <amymishra2@gmail.com>
2020-05-28 19:36:29 +0530

    french language added

--- app/src/main/java/com/javaoneworld/multilangsupport/MainActivity.java
+++ app/src/main/java/com/javaoneworld/multilangsupport/MainActivity.java
@@ -3,12 +3,28 @@ package com.javaoneworld.multilangsupport;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText edtUserEmail, edtUserPassword;
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edtUserEmail = findViewById(R.id.edtEmail);
+        edtUserPassword = findViewById(R.id.edtPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
